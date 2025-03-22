b18f2e583dd6e25961c62913130fe883fba08b69
Laine Hallot <lainehallot@gmail.com>
2025-03-22 15:44:15 -0500

    feat(blog-frontend): create example blog

--- packages/blog-frontend/index.ts
+++ packages/blog-frontend/index.ts
@@ -1,4 +1,4 @@
-import { writeFile, readFile } from 'fs/promises';
+import { writeFile, readFile, readdir } from 'fs/promises';
 import path from 'path';
 import autoprefixer from 'autoprefixer';
 import postcss from 'postcss';
@@ -7,14 +7,14 @@ import tailwindcss from '@tailwindcss/postcss';
 
 export const buildCss = async () => {
   console.log(path.resolve('output/', 'app.css'));
-  const fileData = await readFile('./packages/blog-frontend/style.css', 'utf8');
+  const fileData = await readFile('./style.css', 'utf8');
 
   const postCssResult = await postcss([
     tailwindcss,
     autoprefixer,
     postcssNested,
   ]).process(fileData, {
-    from: './packages/blog-frontend/style.css',
+    from: './style.css',
     to: 'dest/app.css',
   });
 
@@ -22,8 +22,33 @@ export const buildCss = async () => {
 
   if (postCssResult.map) {
     writeFile(
-      path.resolve('../../output/', 'app.css.map'),
+      path.resolve('./output/', 'app.css.map'),
       postCssResult.map.toString()
     );
   }
 };
+
+const blogPosts = await readdir(path.resolve('blog-posts'));
+
+for (const post of blogPosts) {
+  const file = await readFile(path.resolve('blog-posts', post), {
+    encoding: 'utf-8',
+  });
+  const template = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/output/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <article class="prose lg:prose-xl mx-auto">
+        ${file.toString()}
+        </article>
+    </body>
+  </html>`;
+
+  await writeFile(path.resolve('./output/', post), template, 'utf8');
+  await buildCss();
+}
